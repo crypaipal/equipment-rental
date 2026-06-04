9ba5cfd706313d491fe2fcad3c2d016ffbad738e
crypaipal <msamokar228@gmail.com>
2026-06-04 23:46:42 +0200

    Initialized Spring Boot project

---
+++ src/test/java/pl/pwr/miasi/equipmentrental/EquipmentRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pwr.miasi.equipmentrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
